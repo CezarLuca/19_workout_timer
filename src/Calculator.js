c34ce793c5921e360e28bb0b24db3b56af35f99f
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-07-17 21:30:39 +0200

    Setting State Based on Other State Updates done

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,21 +1,38 @@
-import { memo, useState } from "react";
-// import clickSound from './ClickSound.m4a';
+import { memo, useEffect, useState } from "react";
+import clickSound from "./ClickSound.m4a";
 
 function Calculator({ workouts, allowSound }) {
     const [number, setNumber] = useState(workouts.at(0).numExercises);
     const [sets, setSets] = useState(3);
     const [speed, setSpeed] = useState(90);
     const [durationBreak, setDurationBreak] = useState(5);
+    const [duration, setDuration] = useState(0);
 
-    const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+    useEffect(() => {
+        setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
+    }, [number, sets, speed, durationBreak]);
+
+    // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
     const mins = Math.floor(duration);
     const seconds = (duration - mins) * 60;
 
-    // const playSound = function () {
-    //   if (!allowSound) return;
-    //   const sound = new Audio(clickSound);
-    //   sound.play();
-    // };
+    const playSound = function () {
+        if (!allowSound) return;
+        const sound = new Audio(clickSound);
+        sound.play();
+    };
+
+    function handleIncrement() {
+        setDuration((duration) => Math.floor(duration) + 1);
+        playSound();
+    }
+
+    function handleDecrement() {
+        setDuration((duration) =>
+            duration > 1 ? Math.floor(duration) - 1 : 0
+        );
+        playSound();
+    }
 
     return (
         <>
@@ -67,19 +84,25 @@ function Calculator({ workouts, allowSound }) {
                         min="1"
                         max="10"
                         value={durationBreak}
-                        onChange={(e) => setDurationBreak(e.target.value)}
+                        onChange={(e) => {
+                            setDurationBreak(e.target.value);
+                            // setDuration(
+                            //     (number * sets * speed) / 60 +
+                            //         (sets - 1) * e.target.value
+                            // );
+                        }}
                     />
                     <span>{durationBreak} minutes/break</span>
                 </div>
             </form>
             <section>
-                <button onClick={() => {}}>–</button>
+                <button onClick={handleDecrement}>–</button>
                 <p>
                     {mins < 10 && "0"}
                     {mins}:{seconds < 10 && "0"}
                     {seconds}
                 </p>
-                <button onClick={() => {}}>+</button>
+                <button onClick={handleIncrement}>+</button>
             </section>
         </>
     );
