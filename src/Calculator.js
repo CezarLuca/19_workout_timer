87f442c6c5fa57b9e4b7c13491e37db2f72c1f46
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-07-18 21:54:27 +0200

    Using Helper Functions in Effects part 1

--- src/Calculator.js
+++ src/Calculator.js
@@ -8,19 +8,21 @@ function Calculator({ workouts, allowSound }) {
     const [durationBreak, setDurationBreak] = useState(5);
     const [duration, setDuration] = useState(0);
 
-    useEffect(() => {
-        setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
-    }, [number, sets, speed, durationBreak]);
-
-    // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
-    const mins = Math.floor(duration);
-    const seconds = (duration - mins) * 60;
-
     const playSound = function () {
         if (!allowSound) return;
         const sound = new Audio(clickSound);
         sound.play();
     };
+
+    useEffect(() => {
+        setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
+
+        playSound();
+    }, [number, sets, speed, durationBreak, playSound]);
+
+    // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+    const mins = Math.floor(duration);
+    const seconds = (duration - mins) * 60;
 
     function handleIncrement() {
         setDuration((duration) => Math.floor(duration) + 1);
